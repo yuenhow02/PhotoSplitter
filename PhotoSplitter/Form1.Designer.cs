a437cee360c4a743a6b754102de50eebe678e5a7
Yuen How, Chong <yuenhow02@gmail.com>
2020-11-09 23:14:12 +0800

    commit the first code

---
+++ PhotoSplitter/Form1.Designer.cs
@@ -0,0 +1,371 @@
+﻿namespace PhotoSplitter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnLine = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtLineCropH = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtLineCropW = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.btnBrowser = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtOverlapH = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtOverlapW = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 250F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.panel4, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1027, 641);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(253, 3);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(9, 635);
+            this.panel4.TabIndex = 3;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.panel3, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.panel2, 0, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(268, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(756, 635);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.btnExport);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(3, 558);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(750, 74);
+            this.panel3.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(750, 534);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(3, 543);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(750, 9);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.txtOverlapH);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.txtOverlapW);
+            this.panel1.Controls.Add(this.btnLine);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.txtLineCropH);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.txtLineCropW);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txtHeight);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtWidth);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtURL);
+            this.panel1.Controls.Add(this.btnBrowser);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(244, 635);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnLine
+            // 
+            this.btnLine.Location = new System.Drawing.Point(132, 336);
+            this.btnLine.Name = "btnLine";
+            this.btnLine.Size = new System.Drawing.Size(100, 31);
+            this.btnLine.TabIndex = 11;
+            this.btnLine.Text = "DrawLine";
+            this.btnLine.UseVisualStyleBackColor = true;
+            this.btnLine.Click += new System.EventHandler(this.btnLine_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 211);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Height";
+            // 
+            // txtLineCropH
+            // 
+            this.txtLineCropH.Location = new System.Drawing.Point(87, 208);
+            this.txtLineCropH.Name = "txtLineCropH";
+            this.txtLineCropH.Size = new System.Drawing.Size(145, 26);
+            this.txtLineCropH.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 179);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Width";
+            // 
+            // txtLineCropW
+            // 
+            this.txtLineCropW.Location = new System.Drawing.Point(87, 176);
+            this.txtLineCropW.Name = "txtLineCropW";
+            this.txtLineCropW.Size = new System.Drawing.Size(145, 26);
+            this.txtLineCropW.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Height";
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(87, 110);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(145, 26);
+            this.txtHeight.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Width";
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(87, 78);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(145, 26);
+            this.txtWidth.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "File";
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(49, 9);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(183, 26);
+            this.txtURL.TabIndex = 1;
+            // 
+            // btnBrowser
+            // 
+            this.btnBrowser.Location = new System.Drawing.Point(144, 41);
+            this.btnBrowser.Name = "btnBrowser";
+            this.btnBrowser.Size = new System.Drawing.Size(88, 31);
+            this.btnBrowser.TabIndex = 0;
+            this.btnBrowser.Text = "Browes...";
+            this.btnBrowser.UseVisualStyleBackColor = true;
+            this.btnBrowser.Click += new System.EventHandler(this.btnBrowser_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 307);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 20);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Height";
+            // 
+            // txtOverlapH
+            // 
+            this.txtOverlapH.Location = new System.Drawing.Point(87, 304);
+            this.txtOverlapH.Name = "txtOverlapH";
+            this.txtOverlapH.Size = new System.Drawing.Size(145, 26);
+            this.txtOverlapH.TabIndex = 14;
+            this.txtOverlapH.Text = "20";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(31, 275);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 20);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Width";
+            // 
+            // txtOverlapW
+            // 
+            this.txtOverlapW.Location = new System.Drawing.Point(87, 272);
+            this.txtOverlapW.Name = "txtOverlapW";
+            this.txtOverlapW.Size = new System.Drawing.Size(145, 26);
+            this.txtOverlapW.TabIndex = 12;
+            this.txtOverlapW.Text = "20";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 246);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(69, 20);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "OverLap";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(8, 148);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(73, 20);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "CropLine";
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(17, 13);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(118, 46);
+            this.btnExport.TabIndex = 18;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1027, 641);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button btnBrowser;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.Button btnLine;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtLineCropH;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtLineCropW;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtOverlapH;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtOverlapW;
+        private System.Windows.Forms.Button btnExport;
+    }
+}
+
---
+++ PhotoSplitter/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhotoSplitter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowser_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                txtURL.Text = ofd.FileName;
+
+                pictureBox1.Load(ofd.FileName);
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                txtWidth.Text = pictureBox1.Image.Width.ToString();
+                txtHeight.Text = pictureBox1.Image.Height.ToString();
+            }
+        }
+
+        private void btnLine_Click(object sender, EventArgs e)
+        {
+            DrawLine();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            DrawLine();
+        }
+
+        private void DrawLine()
+        {
+            float LineCropWidth = 0;
+            float LineCropHeight = 0;
+
+            float overlapWidth = 0;
+            float overlapHeight = 0;
+
+            if (pictureBox1.Image == null)
+                return;
+
+            if (!float.TryParse(txtLineCropW.Text, out LineCropWidth))
+                return;
+
+            if (!float.TryParse(txtLineCropH.Text, out LineCropHeight))
+                return;
+
+            if (!float.TryParse(txtOverlapW.Text, out overlapWidth))
+                return;
+
+            if (!float.TryParse(txtOverlapH.Text, out overlapHeight))
+                return;
+
+            float ImageSizeWidth = pictureBox1.Image.Width;
+            float ImageSizeHeight = pictureBox1.Image.Height;
+
+            int displayWidth = pictureBox1.Width;
+            int displayHeight = pictureBox1.Height;
+
+            float LineDisplayWidth = (displayWidth * LineCropWidth / ImageSizeWidth);
+            float LineDisplayHeight = (displayHeight * LineCropHeight / ImageSizeHeight);
+
+            float offsetWidth = overlapWidth;
+            float offsetHeight = overlapHeight;
+
+            using (Graphics g = pictureBox1.CreateGraphics())
+            {
+                int ix = 1;
+                while ((LineDisplayHeight * ix) < displayHeight)
+                {
+                    float[] dashValues = { 8, 4, 8, 4 };
+                    Pen pBlack = new Pen(Color.Gray, 1);
+                    pBlack.DashPattern = dashValues;
+                    g.DrawLine(pBlack,0, (ix * LineDisplayHeight), displayWidth, (ix * LineDisplayHeight));
+
+                    g.DrawLine(Pens.Red, 0, (float)((ix * LineDisplayHeight) - offsetHeight), 
+                                displayWidth, (float)((ix * LineDisplayHeight)) - offsetHeight);
+                    g.DrawLine(Pens.Red, 0, (float)((ix * LineDisplayHeight) + offsetHeight), 
+                                displayWidth, (float)((ix * LineDisplayHeight)) + offsetHeight);
+                    ix++;
+                }
+
+                int iy = 1;
+                while ((LineDisplayWidth * iy) < displayWidth)
+                {
+                    float[] dashValues = { 8, 4, 8, 4 };
+                    Pen pBlack = new Pen(Color.Gray, 1);
+                    pBlack.DashPattern = dashValues;
+                    g.DrawLine(pBlack, (iy * LineDisplayWidth), 0, (iy * LineDisplayWidth), displayHeight);
+
+                    g.DrawLine(Pens.Red, (float)((iy * LineDisplayWidth) - offsetWidth), 0,
+                                (float)((iy * LineDisplayWidth) - offsetWidth), displayHeight);
+                    g.DrawLine(Pens.Red, (float)((iy * LineDisplayWidth) + offsetWidth), 0,
+                                (float)((iy * LineDisplayWidth) + offsetWidth), displayHeight);
+                    iy++;
+                }
+            }
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            float LineCropWidth = 0;
+            float LineCropHeight = 0;
+
+            float overlapWidth = 0;
+            float overlapHeight = 0;
+
+            if (pictureBox1.Image == null)
+                return;
+
+            if (!float.TryParse(txtLineCropW.Text, out LineCropWidth))
+                return;
+
+            if (!float.TryParse(txtLineCropH.Text, out LineCropHeight))
+                return;
+
+            if (!float.TryParse(txtOverlapW.Text, out overlapWidth))
+                return;
+
+            if (!float.TryParse(txtOverlapH.Text, out overlapHeight))
+                return;
+
+            float ImageSizeWidth = pictureBox1.Image.Width;
+            float ImageSizeHeight = pictureBox1.Image.Height;
+
+            float offsetWidth = overlapWidth;
+            float offsetHeight = overlapHeight;
+
+            int iy = 0;
+            int Count = 1;
+            while ((LineCropHeight * iy) < ImageSizeHeight)
+            {
+                int ix = 0;
+                while ((LineCropWidth * ix) < ImageSizeWidth) 
+                {
+                    Bitmap target = new Bitmap((int)LineCropWidth, (int)LineCropHeight);
+
+                    using (Graphics g = Graphics.FromImage(target))
+                    {
+                        int startX = (int)(LineCropWidth * ix - offsetWidth);
+                        int startY = (int)(LineCropHeight * iy - offsetHeight);
+
+                        if (startX < 0) startX = 0;
+                        if (startY < 0) startY = 0;
+
+                        g.DrawImage(pictureBox1.Image,
+                                    new Rectangle(0, 0, (int)LineCropWidth, (int)LineCropHeight),
+                                    new Rectangle(startX, startY, (int)LineCropWidth, (int)LineCropHeight), 
+                                    GraphicsUnit.Pixel);
+                    }
+
+                    target.Save("Image" + Count + ".jpg", ImageFormat.Jpeg);
+                    Count++;
+                    ix++;
+                }
+                iy++;
+            }
+
+        }
+    }
+}
